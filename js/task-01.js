581c57ee4b3bf40c523cf6214013f4ddd956e39b
Aleksvoloshyn <alexx.voloshyn@gmail.com>
2021-05-17 22:30:01 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -5,3 +5,9 @@ const items = document.querySelectorAll('.item');
 const itemsQuantity = items.length;
 
 console.log(`В списке ${itemsQuantity} категории`);
+
+/**Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2)
+ *  и количество элементов в категории (всех вложенных в него элементов li). */
+
+const labels = document.querySelectorAll('.item h2');
+const liQuantity = document.querySelectorAll('.item li');
