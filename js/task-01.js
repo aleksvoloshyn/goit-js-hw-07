3c36ba6b4dd3da696d3ef48e8603fbf4d828f656
Aleksvoloshyn <alexx.voloshyn@gmail.com>
2021-05-18 14:19:58 +0300

    task 1 done

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,18 @@
 /*Посчитает и выведет в консоль количество 
 категорий в ul#categories, то есть элементов li.item*/
 
-const items = document.querySelectorAll('.item');
-const itemsQuantity = items.length;
+const itemsEl = document.querySelectorAll('.item');
+const itemsQuantityEl = itemsEl.length;
 
-console.log(`В списке ${itemsQuantity} категории`);
+console.log(`В списке ${itemsQuantityEl} категории`);
 
 /**Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2)
  *  и количество элементов в категории (всех вложенных в него элементов li). */
 
-const labels = document.querySelectorAll('.item h2');
-const liQuantity = document.querySelectorAll('.item li');
+const getCategories = document.querySelectorAll('#categories .item');
+const sortedCategories = getCategories.forEach(el => {
+  const categoryName = el.querySelector('h2').textContent;
+  const liQuantity = el.querySelectorAll('ul li').length;
+  console.log(`Категория: ${categoryName}`);
+  console.log(`Количество элементов: ${liQuantity}`);
+});
