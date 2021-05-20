4f60b2afa802090c657efba8138024c6b621ab4d
Aleksvoloshyn <alexx.voloshyn@gmail.com>
2021-05-20 17:04:37 +0300

    task4 completed

--- js/task-04.js
+++ js/task-04.js
@@ -1,3 +1,17 @@
+let counterValue = document.querySelector('#value');
 
+const decrement = () => {
+  return (counterValue.textContent -= 1);
+};
 
-const counterValue = document.querySelector('#value')
+const increment = () => {
+  return (counterValue.textContent = +counterValue.textContent + 1);
+};
+
+const but = document.querySelectorAll('button');
+
+const decrementBtn = but[0];
+const incrementBtn = but[1];
+
+decrementBtn.addEventListener('click', decrement);
+incrementBtn.addEventListener('click', increment);
