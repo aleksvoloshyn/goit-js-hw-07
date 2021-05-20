0206a2afc8702136e6fb525e82e13eb3b5e44c99
Aleksvoloshyn <alexx.voloshyn@gmail.com>
2021-05-21 00:22:53 +0300

    8 not finished

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const inputEl = document.querySelector('input');
 
+const divBoxesEl = document.querySelector('#boxes');
+const renderButtonEl = document.querySelector('[data-action="render"]');
+const destroyButtonEl = document.querySelector('[data-action="destroy"]');
+
+const htmlTemplateEl = `<div>test</div>`;
+
+const createBoxes = amount => {
+  amount = inputEl.value;
+  for (let i = 0; i < amount; i += 1) {
+    divBoxesEl.insertAdjacentHTML('beforeend', htmlTemplateEl);
+    divBoxesEl.firstElementChild.classList.add('quoter');
+  }
+};
+
+const destroyBoxes = () => {
+  divBoxesEl.innerHTML = '';
+};
+
+renderButtonEl.addEventListener('click', createBoxes);
+
+destroyButtonEl.addEventListener('click', destroyBoxes);
